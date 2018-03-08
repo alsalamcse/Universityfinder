aeeb825d46c572fa24f13cba540bf86239ea745e
user <adhamkl2000@gmail.com>
2018-03-08 11:32:16 +0200

    adham

--- app/src/main/java/com/afeefy/sara/universityfinder/Houses.java
+++ app/src/main/java/com/afeefy/sara/universityfinder/Houses.java
@@ -1,5 +1,4 @@
 package com.afeefy.sara.universityfinder;
-
 import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +12,6 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.afeefy.sara.universityfinder.Data.House;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,9 +20,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.List;
-
 public class Houses extends AppCompatActivity implements View.OnClickListener {
     private ImageButton IMGADD;
     private TextView ETNUMHOUSE;
@@ -33,6 +29,8 @@ public class Houses extends AppCompatActivity implements View.OnClickListener {
     private ListView LVHOUSES;
     private HouseAdapter houseAdapter;
     private Button BTNSAVE;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
 
 
     protected void onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -49,11 +47,11 @@ public class Houses extends AppCompatActivity implements View.OnClickListener {
         BTNSAVE = (Button) findViewById(R.id.BTNSAVE);
         houseAdapter = new HouseAdapter(this, R.layout.item_house);
         LVHOUSES.setAdapter(houseAdapter);
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+
         readAndListen();
-
-
     }
-
     private void readAndListen() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
@@ -61,44 +59,39 @@ public class Houses extends AppCompatActivity implements View.OnClickListener {
         email = email.replace('.', '*');
         DatabaseReference reference;
         reference = FirebaseDatabase.getInstance().getReference();
-        reference.child(email).child("mylist");
-        addValueEventListener(new ValueEventListener() {
+        reference.child(email).child("mylist").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {
+            public void onDataChange(DataSnapshot dataSnapshot) {
                 houseAdapter.clear();
-                for (DataSnapshot ds:dataSnapshot.getChildren())
-                {
-                    House h=ds.getValue(House.class);
-                    Log.d("SL",h.toString());
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    House h = ds.getValue(House.class);
+                    Log.d("SL", h.toString());
                     houseAdapter.add(h);
+                }
             }
-        }
-
             @Override
             public void onCancelled(DatabaseError databaseError)
             {
-
-            };
-
-
-
-
-
-        @Override
-        public void onClick (View v)
-        {
-            if (IMGADD == v) {
-                Intent i = new Intent(this, Addhouse.class);
-                startActivity(i);
             }
+        });
+    }
+    @Override
+    public void onClick(View v)
+    {
+        if (IMGADD == v)
+        {
+            Intent i = new Intent(this, Addhouse.class);
+            startActivity(i);
         }
-
-
     }
 
-
 }
+
+
+
+
+
+
 
 
 
