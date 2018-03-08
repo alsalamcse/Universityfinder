b2254df2264e79c852e5506848de0a1865ca1910
user <adhamkl2000@gmail.com>
2018-03-08 09:41:15 +0200

    adham

--- app/src/main/java/com/afeefy/sara/universityfinder/Houses.java
+++ app/src/main/java/com/afeefy/sara/universityfinder/Houses.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,8 @@ import android.widget.TextView;
 import com.afeefy.sara.universityfinder.Data.House;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -32,50 +35,70 @@ public class Houses extends AppCompatActivity implements View.OnClickListener {
     private Button BTNSAVE;
 
 
-    protected void onCreate(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+    protected void onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view=inflater.inflate(R.layout.activity_houses,container,false);
+        View view = inflater.inflate(R.layout.activity_houses, container, false);
         setContentView(R.layout.activity_houses);
-        IMGADD= (ImageButton) findViewById(R.id.IMGADD);
-        LVHOUSES= (ListView) findViewById(R.id.LVHOUSES);
-        ETNUMHOUSE= (TextView) findViewById(R.id.ETNUMHOUSE);
-        ETSUBJECT= (TextView) findViewById(R.id.ETSUBJECT);
-        ETYEAR= (TextView) findViewById(R.id.ETYEAR);
-        ETSPACE= (TextView) findViewById(R.id.ETSPACE);
-        ETPHONENUMBER= (TextView) findViewById(R.id.ETPHONENUMBER);
-        BTNSAVE= (Button) findViewById(BTNSAVE);
-
-        houseAdapter=new HouseAdapter(R.layout.item_house);
-        LVHOUSES.setAdapter(HouseAdapter);
+        IMGADD = (ImageButton) findViewById(R.id.IMGADD);
+        LVHOUSES = (ListView) findViewById(R.id.LVHOUSES);
+        ETNUMHOUSE = (TextView) findViewById(R.id.ETNUMHOUSE);
+        ETSUBJECT = (TextView) findViewById(R.id.ETSUBJECT);
+        ETYEAR = (TextView) findViewById(R.id.ETYEAR);
+        ETSPACE = (TextView) findViewById(R.id.ETSPACE);
+        ETPHONENUMBER = (TextView) findViewById(R.id.ETPHONENUMBER);
+        BTNSAVE = (Button) findViewById(R.id.BTNSAVE);
+        houseAdapter = new HouseAdapter(this, R.layout.item_house);
+        LVHOUSES.setAdapter(houseAdapter);
         readAndListen();
-        return view;
+
+
     }
 
-    private void readAndListen()
-    {
-        FirebaseAuth auth=FirebaseAuth.getInstance();
-        FirebaseUser user=auth.getCurrentUser();
-        String email=user.getEmail();
-        email=email.replace('.','*');
+    private void readAndListen() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        String email = user.getEmail();
+        email = email.replace('.', '*');
         DatabaseReference reference;
-        reference= FirebaseDatabase.getInstance().getReference();
+        reference = FirebaseDatabase.getInstance().getReference();
         reference.child(email).child("mylist");
-
-    }
-
-    @Override
-    public void onClick(View v)
-    {
-        if (IMGADD==v)
-        {
-            Intent i=new Intent(this,Addhouse.class);
-            startActivity(i);
+        addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                houseAdapter.clear();
+                for (DataSnapshot ds:dataSnapshot.getChildren())
+                {
+                    House h=ds.getValue(House.class);
+                    Log.d("SL",h.toString());
+                    houseAdapter.add(h);
+            }
         }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+            };
+
+
+
+
+
+        @Override
+        public void onClick (View v)
+        {
+            if (IMGADD == v) {
+                Intent i = new Intent(this, Addhouse.class);
+                startActivity(i);
+            }
+        }
+
+
     }
 
 
-
-    }
+}
 
 
 
